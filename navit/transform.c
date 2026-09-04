e8ea901482b890a46c55eacf868bf9a679df008e
Christof Schulze <christof.schulze@gmx.net>
2026-09-04 17:36:40 +0200

    fix(build): add missing includes and rename near/far for wince
    Add <math.h> to coord.h and rename the near/far parameters of
    transform_reverse_near_far to near_dist/far_dist.
    
    windows.h (windef.h) defines `near` and `far` as macros for Win32/CE,
    so using them as identifiers expands them away and breaks the wince
    build; renaming avoids the macro collision.

--- navit/transform.c
+++ navit/transform.c
@@ -662,13 +662,14 @@ static void transform_screen_to_3d(struct transformation *t, struct point *p, na
     cg->z = z - offz;
 }
 
-static int transform_reverse_near_far(struct transformation *t, struct point *p, struct coord *c, int near, int far) {
+static int transform_reverse_near_far(struct transformation *t, struct point *p, struct coord *c, int near_dist,
+                                      int far_dist) {
     double xc, yc;
     dbg(lvl_debug, "%d,%d", p->x, p->y);
     if (t->ddd) {
         struct coord_geo_cart nearc, farc, nears, fars, intersection;
-        transform_screen_to_3d(t, p, near, &nearc);
-        transform_screen_to_3d(t, p, far, &farc);
+        transform_screen_to_3d(t, p, near_dist, &nearc);
+        transform_screen_to_3d(t, p, far_dist, &farc);
         transform_apply_inverse_matrix(t, &nearc, &nears);
         transform_apply_inverse_matrix(t, &farc, &fars);
         if (transform_zplane_intersection(&nears, &fars, HOG(*t), &intersection) != 1)
